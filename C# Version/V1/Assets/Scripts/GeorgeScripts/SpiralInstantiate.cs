50e4925e651374088c6e57232cfe0cee0780a3d7
George Brignell-Cash <georgebrignellcash@gmail.com>
2023-02-02 14:54:41 +0000

    Double spiral

--- C# Version/V1/Assets/Scripts/GeorgeScripts/SpiralInstantiate.cs
+++ C# Version/V1/Assets/Scripts/GeorgeScripts/SpiralInstantiate.cs
@@ -14,22 +14,49 @@ public class SpiralInstantiate : MonoBehaviour
 
     void Awake()
     {
-        for (int i = 0; i < spawnCount; i++)
+        //Double spiral
+        for (int i = 0; i < spawnCount/2; i++)
         {
             float theta = ((2 * Mathf.PI * i) / spawnCount) * numRounds ;
             float r = theta * theta;
-            float x = radius * r * Mathf.Cos(theta) + Random.Range(-noise, noise);
-            float z = radius * r * Mathf.Sin(theta) + Random.Range(-noise, noise);
+            float x = radius * r * Mathf.Cos(theta);
+            float z = radius * r * Mathf.Sin(theta);
 
             Vector3 pos;
-            pos.x = x;
-            pos.z = z;
+            pos.x = x + Random.Range(-noise, noise);
+            pos.z = z + Random.Range(-noise, noise);
             pos.y = 50 + Random.Range(-y_range, y_range); //Might want to make this range a negative probability, dont want a flat top
 
-            Star star = Instantiate(prefab);
-            star.transform.position = pos;
-    
+            Star star1 = Instantiate(prefab);
+            star1.transform.position = pos;
+
+
+            pos.x = -x + Random.Range(-noise, noise);
+            pos.z = -z + Random.Range(-noise, noise);
+            pos.y = 50 + Random.Range(-y_range, y_range);
+
+            Star star2 = Instantiate(prefab);
+            star2.transform.position = pos;
+
         }
+
+        //One spiral
+        // for (int i = 0; i < spawnCount; i++)
+        // {
+        //     float theta = ((2 * Mathf.PI * i) / spawnCount) * numRounds ;
+        //     float r = theta * theta;
+        //     float x = radius * r * Mathf.Cos(theta) + Random.Range(-noise, noise);
+        //     float z = radius * r * Mathf.Sin(theta) + Random.Range(-noise, noise);
+
+        //     Vector3 pos;
+        //     pos.x = x;
+        //     pos.z = z;
+        //     pos.y = 50 + Random.Range(-y_range, y_range); //Might want to make this range a negative probability, dont want a flat top
+
+        //     Star star = Instantiate(prefab);
+        //     star.transform.position = pos;
+    
+        // }
 
     }
 }
