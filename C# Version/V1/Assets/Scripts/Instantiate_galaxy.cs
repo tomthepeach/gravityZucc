a4a753c4e007aa60b0a118233042bdcc2bdcf492
George Brignell-Cash <georgebrignellcash@gmail.com>
2023-02-02 23:07:54 +0000

    Added disc spawner (need combine) and removed +50y

--- C# Version/V1/Assets/Scripts/Instantiate_galaxy.cs
+++ C# Version/V1/Assets/Scripts/Instantiate_galaxy.cs
@@ -11,6 +11,7 @@ public class Instantiate_galaxy : MonoBehaviour
     public int y_range = 10;
     public int galaxyNoise = 10;
     public float galaxyRadius = 1000.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +26,7 @@ public class Instantiate_galaxy : MonoBehaviour
             // star_i.GetComponent<Rigidbody>().mass = inpmass;
             // star_i.GetComponent<Rigidbody>().velocity = inpinitialVelocity;
 
-            float theta = ((4 * Mathf.PI * i) / starCount) * numRounds ;
+            float theta = ((2 * 2 * Mathf.PI * i) / starCount) * numRounds ;
             float r = theta * theta;
             float x = galaxyRadius * r * Mathf.Cos(theta);
             float z = galaxyRadius * r * Mathf.Sin(theta);
@@ -33,16 +34,15 @@ public class Instantiate_galaxy : MonoBehaviour
             Vector3 pos;
             pos.x = x + Random.Range(-galaxyNoise, galaxyNoise);
             pos.z = z + Random.Range(-galaxyNoise, galaxyNoise);
-            pos.y = 50 + Random.Range(-y_range, y_range); //Might want to make this range a negative probability, dont want a flat top
+            pos.y = Random.Range(-y_range, y_range); //Dont want a flat top -Use gaussian
 
             GameObject star1 = Instantiate(Star);
             star1.transform.position = pos;
             star1.transform.localScale = starRadius;
 
-
             pos.x = -x + Random.Range(-galaxyNoise, galaxyNoise);
             pos.z = -z + Random.Range(-galaxyNoise, galaxyNoise);
-            pos.y = 50 + Random.Range(-y_range, y_range);
+            pos.y = Random.Range(-y_range, y_range);
 
             GameObject star2 = Instantiate(Star);
             star2.transform.position = pos;
