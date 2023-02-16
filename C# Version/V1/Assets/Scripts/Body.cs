5938074db785e2e9ca1a3d6a8c95645f086a41fc
George Brignell-Cash <georgebrignellcash@gmail.com>
2023-02-16 12:42:07 +0000

    Spring Clean

--- C# Version/V1/Assets/Scripts/Body.cs
+++ C# Version/V1/Assets/Scripts/Body.cs
@@ -7,68 +7,82 @@ public class Body : MonoBehaviour
 {
 	public Vector3 netForce = Vector3.zero;
 	public Vector3 velocity = Vector3.zero;
-	float scalar_vel;
-	public float ke;
-	public float pe;
 
-	public int blackhole = 0; // 1 for bh. 0 for star
 
-	public float mass = 1;
+	private const float GRAVITY_CONST = 1f;
+	public float mass = 1f;
+	public static float scale = 1f;
 
-	public float radius = 1;
+	public Vector3 scale3D = Vector3.one * scale;
 
-	public void init(float inpmass, Vector3 inpinitialVelocity, float inpRadius, int bh)
+	public static List<Body> Bodies;
+
+
+	// Use this for initialization
+	void Start()
 	{
-		blackhole = bh;
-		mass = inpmass;
-		velocity = inpinitialVelocity;
-		radius = inpRadius;
-		transform.localScale = Vector3.one * inpRadius * 2;
+		if (Bodies == null)
+			Bodies = new List<Body>();
+
+		Bodies.Add(this);
 		
 	}
 
 	// Update is called once per frame
-	public void UpdateVelocity(List<Body> Bodies, float timestep)
+	void FixedUpdate()
 	{
-		Vector3 peV = Vector3.zero;
-		float scalar_vel = velocity.magnitude;
-		ke = scalar_vel * scalar_vel * 0.5f * mass;
+
+
+		foreach (Body _body in Bodies.ToArray())
+		{
+			if (_body == this)
+				continue;
+
+			// distance between bodies
+			float r = Vector3.Distance(transform.position, _body.transform.position);
+
+			if (r < scale)
+			{
+				mass += _body.mass;
+				transform.Translate((_body.transform.position - transform.position) / 2);
+				Debug.Log("Here");
+				velocity += _body.velocity;
+				// size 
+
+				Destroy(_body.gameObject, 0);
+				Bodies.Remove(_body);
+
+			}
+		}
+
+
 
 		foreach (Body _body in Bodies)
 		{
+			if (_body == this)
+				continue;
 
-			if (_body != this)
-			{
-				// distance between bodies
-				float r = Vector3.Distance(transform.position, _body.transform.position);
+			// distance between bodies
+			float r = Vector3.Distance(transform.position, _body.transform.position);
 
-				// part of grav formula
-				float F_amp = (mass * _body.mass) / Mathf.Pow(r, 2);
-				
-				// dir 
-				Vector3 dir = Vector3.Normalize(_body.transform.position - transform.position);
 
-				Vector3 F = (dir * F_amp);
-				netForce += F;
-				peV += F * r;
-			}
+			// part of grav formula
+			float F_amp = (mass * _body.mass) / Mathf.Pow(r, 2);
+			
+			// dir 
+			Vector3 dir = Vector3.Normalize(_body.transform.position - transform.position);
+
+			Vector3 F = (dir * F_amp) * Time.fixedDeltaTime;
+			netForce += F;
+
 		}
-		
-		peV *= Constants.BIGG;
 
-		pe = -peV.magnitude;
 
-		netForce *= Constants.BIGG;
+		netForce *= GRAVITY_CONST;
 
-        velocity += netForce/mass * Time.fixedDeltaTime * Time.timeScale;
-             
-        netForce = Vector3.zero;
+		velocity += netForce * Time.fixedDeltaTime;
+
+		transform.Translate(velocity);
+		netForce = Vector3.zero;
 	}
-
-	public void UpdatePosition(float timeStep)
-	{
-		// Debug.Log(velocity);
-		transform.Translate(velocity*timeStep);
-	}
-
 }
--- C# Version/V1/Assets/Scripts/NewBody.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class NBody : MonoBehaviour
-{
-    public float mass;
-    public Vector3 acceleration;
-    public Vector3 velocity;
-}
