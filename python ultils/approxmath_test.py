d172a7431e13613cd2048e37b93433b540f28e3d
tomthepeach <tp17957@bristol.ac.uk>
2023-03-08 20:52:48 +0000

    syncing python analysis

--- python ultils/approxmath_test.py
+++ python ultils/approxmath_test.py
@@ -2,6 +2,7 @@
 import random
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
 def gaussian(mean = 0.0, stdDev = 1.0):
     #Box-Muller transform
@@ -11,12 +12,18 @@ def gaussian(mean = 0.0, stdDev = 1.0):
     result = mean + stdDev * randStdNormal #random normal(mean,stdDev^2)
     return result
 
-def rayleigh(sigma=1.0):
-    return sigma*(-2*math.log2(random.random()))**0.5
+def rayleigh(sigma=0.1):
+    return (sigma*(-2*math.log2(random.random()))**0.5)
 
-vals = [rayleigh(0.5) for i in range(10000)]
+def bounded_gaussian(mean = 0.0, stdDev = 1.0, lowerBound = -1.0, upperBound = 1.0):
+    while True:
+        result = gaussian(mean, stdDev)
+        if result >= lowerBound and result <= upperBound:
+            return result
+
+vals = [bounded_gaussian(0.5, 40, 0, 150) for i in range(10000)]
 
 plt.hist(vals, bins=100)
-
+print(np.mean(vals))
 
 # %%
--- python ultils/headset-data.py
+++ python ultils/headset-data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 df = pd.read_csv(r'C:\Users\tompe\Desktop\Final project code\csv\com.UniversityofBristol.V1-20230306_163601.csv')
 
-df['gpu_utilization_percentage'].plot()
+df.columns
 # %%
-df['cpu_utilization_percentage'].plot()
+df['display_refresh_rate'].plot()
 # %%
