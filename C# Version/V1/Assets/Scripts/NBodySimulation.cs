22ea1e10797ec79b0644301a60368c806090c0a1
tomthepeach <tp17957@bristol.ac.uk>
2023-03-21 15:37:48 +0000

    added real lumins

--- C# Version/V1/Assets/Scripts/NBodySimulation.cs
+++ C# Version/V1/Assets/Scripts/NBodySimulation.cs
@@ -84,7 +84,7 @@ public class NBodySimulation : MonoBehaviour
                                 starMat.SetColor("_EmissionColor", starCol);
                                 starMat.SetFloat("_Luminosity", 1000000f);
 
-                                float baseLumin = 10f;  // this should depend on the star's mass. and should be a public varible tht is updated on collision rather than calc-ed at every update
+                                float baseLumin = ApproxMath.massLuminosity(this_body.mass);  // this should depend on the star's mass. and should be a public varible tht is updated on collision rather than calc-ed at every update
                                 StartCoroutine(UpdateLuminosity(this_body, baseLumin));
                             }
                         }
