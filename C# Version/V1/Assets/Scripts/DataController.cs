3f1b041a2a457b816a9a49286039a1e9e6b5ea17
geobrignell <89096835+geobrignell@users.noreply.github.com>
2023-04-12 22:29:04 +0100

    trying pe ke

--- C# Version/V1/Assets/Scripts/DataController.cs
+++ C# Version/V1/Assets/Scripts/DataController.cs
@@ -11,11 +11,10 @@ public class DataController
 
     public List<List<float>> masses = new List<List<float>>();
     public List<float> times = new List<float>();
+    public List<float> potentials = new List<float>();
+    public List<float> kinetics = new List<float>();
 
-    // For fps vs starcount graphs
-    //public List<float> FPS = new List<float>();
-    //public List<int> numberOfStars = new List<int>();
-
+ 
     public void SaveData()
     {
         Debug.Log("Saving data to file..."+ Directory.GetCurrentDirectory());
@@ -23,13 +22,13 @@ public class DataController
 
         // Save data to file
         StreamWriter writer = new StreamWriter("sim_data_" + currentDateTime.ToString("yyyy-MM-dd_HH-mm-ss") + ".csv");
-        writer.WriteLine("time,index,mass,x,y,z,vx,vy,vz,fx,fy,fz");
+        writer.WriteLine("time,index,mass,x,y,z,vx,vy,vz,fx,fy,fz,ke,pe");
 
         for(int i=0; i < times.Count; i++)
         {
             for (int j = 0; j < masses[i].Count; j++)
             {
-                string line = string.Join(",", times[i], j, masses[i][j], positions[i][j][0], positions[i][j][1], positions[i][j][2], velocities[i][j][0], velocities[i][j][1], velocities[i][j][2], forces[i][j][0], forces[i][j][1], forces[i][j][2]);
+                string line = string.Join(",", times[i], j, masses[i][j], positions[i][j][0], positions[i][j][1], positions[i][j][2], velocities[i][j][0], velocities[i][j][1], velocities[i][j][2], forces[i][j][0], forces[i][j][1], forces[i][j][2],KESOMETHING[i][j],PESOMETHING[i][j]);
                 writer.WriteLine(line);
             }
         }
@@ -46,28 +45,9 @@ public class DataController
         List<Vector3> force = new List<Vector3>();
         List<float> mass = new List<float>();
 
-    // public void SaveFPSData()
-    // {
-    //     DateTime currentDateTime = DateTime.Now;
-
-    //     // Save data to file
-    //     StreamWriter writer = new StreamWriter("fps_data_" + currentDateTime.ToString("yyyy-MM-dd_HH-mm-ss") + ".csv");
-    //     writer.WriteLine("time,numberOfStars,FPS");
-
-    //     for(int i=0; i < times.Count; i++)
-    //     {
-    //         string line = string.Join(",", times[i], numberOfStars[i], FPS[i]);
-    //             writer.WriteLine(line);
-    //     }
-
-    //     writer.Flush();
-    //     writer.Close();
-    // }
-
-
-
-//     public void AddData(float time, List<Body> bodies)
-//     {
+   
+   public void AddData(float time, List<Body> bodies)
+     {
 
         foreach (Body body in bodies)
         {
